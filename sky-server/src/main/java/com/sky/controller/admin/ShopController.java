fe933e6d57a4bc6e58a140bac0c07db9a1c6971a
lvshrd <lizepu2020@outlook.com>
2025-02-06 15:32:54 +0800

    set shop status(open or close)

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,44 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+
+    public static final String KEY = "shop_status";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺营业状态
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺营业状态")
+    public Result setStatus(@PathVariable Integer status) {
+        log.info("设置店铺营业状态 为: {}",status==1? "营业中" : "已打烊");
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus() {
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        return Result.success(status);
+    }
+}
